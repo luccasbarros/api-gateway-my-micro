70b26fade0292ebaf4a9f316fc2614cfad64ac2a
Luccas Barros <luccasbarros@pm.me>
2022-02-23 12:05:33 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,24 @@ import * as momentTimezone from 'moment-timezone';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
 import { TimeoutInterceptor } from './common/interceptor/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Wallet Microservice')
+    .setDescription('Wallet microservice')
+    .setVersion('1.0')
+    .addTag('Transactions')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
 
   Date.prototype.toJSON = function (): any {
     return momentTimezone(this)
